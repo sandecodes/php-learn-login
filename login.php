f95f62fe81f92e315470de06fc0cd5f735541e2f
sandecodes <muhammadsandy74@gmail.com>
2022-06-12 11:25:20 +0700

    add style login page with bootstrap

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ session_start();
 require 'koneksi.php';
 
 if (isset($_SESSION['login'])) {
-    header("Location: index.php");
+    header("Location: ./index");
     exit;
 }
 
@@ -18,7 +18,7 @@ if (isset($_POST['login'])) {
     if (mysqli_num_rows($result) > 0) {
         $_SESSION['username'] = $username;
         $_SESSION['login'] = true;
-        header("Location: index.php");
+        header("Location: ./index");
         exit;
     }
 
@@ -29,34 +29,43 @@ if (isset($_POST['login'])) {
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
-
 <body>
-    <?php if (isset($error)) : ?>
-        <p style="color: red; font-style: italic;">Username atau password anda salah</p>
-    <?php endif; ?>
-    <form action="" method="POST">
-        <table>
-            <tr>
-                <td>Username</td>
-                <td><input type="text" name="username" id="" autofocus autocomplete></td>
-            </tr>
-            <tr>
-                <td>Password</td>
-                <td><input type="password" name="password" id=""></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><button type="submit" name="login">Login!</button></td>
-            </tr>
-        </table>
-    </form>
-</body>
 
+    <div class="container box d-flex justify-content-center align-items-center">
+        <div class="box-center">
+            <form method="post" action="">
+            <?php if (isset($error)) : ?>
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    Username atau Password anda salah!
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            <?php endif; ?>
+                <div class="mb-3">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control" name="username" id="username" autofocus autocomplete="off">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" name="password" id="password">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Remember Me!</label>
+                </div>
+                <button type="submit" name="login" class="btn btn-primary">Login!</button>
+                <button type="reset" class="btn btn-danger">Cancel!</button>
+            </form>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
 </html>
--- logout.php
+++ logout.php
@@ -3,6 +3,6 @@ session_start();
 $_SESSION = [];
 session_unset();
 session_destroy();
-header("Location: login.php");
+header("Location: ./login");
 exit;
 ?>
